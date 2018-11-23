983603adfbbe0bd1a3370f55192b2d80c1df30a3
SIMSEUNGMIN <ssm4591@naver.com>
2018-11-23 20:16:34 +0900

    곱하기(mul()), 나누기(div()) 추가

--- Calculator.java
+++ Calculator.java
@@ -3,7 +3,15 @@ public class Calculator{
   public int sum(int a,int b){
     return a+b;
   }
-  public int minus(int, int b){
+  public int minus(int a, int b){
     return a-b;
+  }
+
+  public int mul(int a, int b){
+    return a*b;
+  }
+
+  public int div(int a, int b){
+    return a / b;
   }
 }
